6963480856ee6866510deb348ed70a2d484715c3
Akhmedov-Asad <akhmedovasadbek76@gmail.com>
2023-02-07 15:04:38 +0500

    homework 32

---
+++ js/script.js
@@ -0,0 +1,49 @@
+/* n1 */
+
+// let years = [1985, 1995, 2000, 2004, 1950]; /* years arr */
+// let yearsArr = (presentYear) => {
+//     return presentYear - 2023;
+// }
+// for(let i = 0; i < years.length; i++){
+//     console.log(yearsArr(years[i]));
+// }
+/*  <=--------------------------------------------------------------------------------------------=> */
+
+/* n2 */
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let numbersFun = (multiplicationNumber) => {
+    //     return multiplicationNumber * 2;
+    // }
+    // for (let j = 0; j < numbers.length; j++) {
+        //     console.log(numbersFun(numbers[j]));
+        // }
+
+
+
+/* shu masala yechish sal kiynro bovotti, chushinmyman vazifa da yozilgan narsalaga((( */
+
+
+/* qoshimcha */
+        //  method(push)
+//   let arrPush = ['apple', 'mango', 'orange'];
+//   console.log(arrPush);
+//   arrPush.push(['banana', 'cherry']);
+//   console.log(arrPush);
+
+        //  method(unshift
+//    let arrConcat = ['James', 'Robert', 'John', 'Polson'];
+//     console.log(arrConcat);
+//     arrConcat.unshift('Fermino', 'Salah');
+//     console.log(arrConcat);   /* ohiriga qoshvoradi */
+
+ //  method(unshift
+
+    //  let students = ['James', 'Robert', 'John', 'Polson'];
+    //  console.log(students);
+
+    //  let newStudents = students.slice(3);
+    //  console.log(newStudents);   /* ikta argument qabul qvotti, qaysi element dan qaysi elementga gachan kesib oladi */
+
+
+    
